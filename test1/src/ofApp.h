82857f7f3cacedb5b373693af4e90953ace4124e
SalamiSupream <c_e_y_a_n@hotmail.de>
2015-11-12 01:53:47 +0100

    aufräumen

---
+++ test1/src/ofApp.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ofxiOS.h"
+
+class ofApp : public ofxiOSApp{
+	
+    public:
+    int myArray[];
+        void setup();
+        void update();
+        void draw();
+        void exit();
+
+};
+
+
